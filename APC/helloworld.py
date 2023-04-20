e1cceb1926e2ae90a07699960682500fb22f0ae2
Wagner <luiswagner451@gmail.com>
2023-04-20 09:05:07 -0300

    -A

--- APC/helloworld.py
+++ APC/helloworld.py
@@ -1,8 +1,9 @@
-def somalista(numeros):
-    soma = 0
-    for i in numeros:
-        soma = soma + i
-    return soma
+print("Qual o tamanho do vosso for?", '\n')
+tamanho = int(input())
+soma = 0
 
+for i in range(tamanho):
+    numbers = int(input())
+    soma = soma + numbers
 
-print(somalista([1, 3, 5, 7, 9]))
+print('\n', 'Vossa soma deu o valor =', soma)
