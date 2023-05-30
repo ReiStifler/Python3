fe1638010887c945252b280357184782aef17f76
Wagner <luiswagner451@gmail.com>
2023-05-30 08:40:16 -0300

    mais_velho

---
+++ Lista_Seq/10.py
@@ -0,0 +1,7 @@
+def older(ageA, ageB):
+    if int(ageA) > int(ageB):
+        return print("A")
+    elif int(ageA) < int(ageB):
+        return print("B")
+    if int(ageA) == int(ageB):
+        return print("Maybe twins")
